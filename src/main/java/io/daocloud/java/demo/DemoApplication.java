5df6e138fb47e9ca0baecb6e0273ef1314a0f8ff
yann <me.yan.xia@qq.com>
2017-11-14 21:51:10 +0800

    first commit

--- src/main/java/io/daocloud/java/demo/DemoApplication.java
+++ src/main/java/io/daocloud/java/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package io.daocloud.java.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,13 @@ import java.time.LocalDateTime;
 
 @RestController
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
